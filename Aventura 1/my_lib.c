d981c2d403e750227896858c76e15528016f9fcf
Felix Aguilar Ferrer <aguilarferrerfelix@gmail.com>
2019-10-05 17:57:16 +0200

    Adiciones y resolución de problemas
    Funcionamiento correcto falta poner comentarios

--- Aventura 1/my_lib.c
+++ Aventura 1/my_lib.c
@@ -1,9 +1,10 @@
-//
 //  my_lib.c
-//  
 //
-//  Created by Felix on 01/10/2019.
+//  Created by Felix Aguilar Ferrer
+//             Adrian Bennassar Polzin
+//             Alvaro ...
 //
+//  on 01/10/2019.
 
 #include <stdio.h>
 
@@ -13,8 +14,45 @@ size_t my_strlen(const char * str){
     return i;
 }
 
-char my_strcat(char *dest, const char *src){
+int my_strcmp(const char *str1, const char *str2){
+    
+    for(int i=0; i < (my_strlen(str1)&&(my_strlen(str2)));i++){
+        if((*(str1+i)) != (*(str2+i))){
+            int resta = *(str1+i) - *(str2+i);
+            return resta;
+        }
+    }
+    return 0;
     
 }
 
+char *my_strcpy(char *dest, const char *src){
+    size_t longitudSrc = my_strlen(src);
+    for(int i=0; i < longitudSrc; i++){
+        *(dest+i)= *(src+i);
+    }
+    *(dest+longitudSrc) = '\0';
+    return dest;
+}
 
+char *my_strncpy(char *dest, const char *src, size_t n){
+    size_t longitudSrc = my_strlen(src);
+    for(int i=0; (i < n)&&(i < longitudSrc); i++){
+        *(dest+i)= *(src+i);
+    }
+    *(dest+n+1) = '\0';
+    return dest;
+}
+
+char *my_strcat(char *dest, const char *src){
+    
+    size_t d = my_strlen(dest);
+    size_t s = my_strlen(src);
+    
+    for (int i = 0; i < s; i++) {
+        *(dest+d+i) = *(src+i);
+    }
+    *(dest+d+s) = '\0';
+    
+    return dest;
+}
