6d547cbd04a41f523bab1d980b25ed36852c040b
Adrián Bennasar Polzin <49955569+Krymsone@users.noreply.github.com>
2019-10-05 11:58:12 +0200

    Función strcomp

---
+++ Aventura 1/strcomp.c
@@ -0,0 +1,28 @@
+/* función equivalente a strcomp() */
+#include <stdio.h>
+
+size_t my_strlen(const char * str);
+
+int main(){
+    return 0;
+}
+
+int my_strcmp(const char *str1, const char *str2){
+
+for(int i=0; i < (my_strlen(str1)&&(my_strlen(str2)));i++){
+    if((*(str1+i)) != (*(str2+i))){
+        int resta = *(str1+i) - *(str2+i);
+        return resta;
+    }
+}
+    return 0;
+
+}
+
+
+size_t my_strlen(const char * str){
+    size_t i;
+    for(i = 0; *(str + i) != '\0'; i++){}
+    return i;
+}
+
